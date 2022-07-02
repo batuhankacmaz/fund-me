f2e25cc7d36cd44b9357323fa5651db7cfefbb27
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2022-07-02 09:44:37 +0300

    contract deploy

---
+++ helper-hardhat-config.ts
@@ -0,0 +1,21 @@
+export interface networkConfigItem {
+    ethUsdPriceFeed?: string
+    blockConfirmations?: number
+}
+
+export interface networkConfigInfo {
+    [key: string]: networkConfigItem
+}
+
+export const networkConfig: networkConfigInfo = {
+    localhost: {},
+    hardhat: {},
+    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+    // Default one is ETH/USD contract on Rinkeby
+    rinkeby: {
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        blockConfirmations: 6,
+    },
+}
+
+export const developmentChains = ["hardhat", "localhost"]
